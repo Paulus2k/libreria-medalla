255980d7540a7cf525acd50166b1f76938a8d654
Paulus2k <pcardonas@miumg.edu.gt>
2026-05-21 17:07:13 -0600

    fix CORS production

--- libreria-backend/server.js
+++ libreria-backend/server.js
@@ -5,7 +5,12 @@ const cors    = require("cors");
 const app  = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://imaginative-determination-production-0818.up.railway.app'
+  ]
+}))
 app.use(express.json());
 
 // ── Rutas ──
